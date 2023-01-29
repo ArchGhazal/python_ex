0c1b0b2fa9212492be6e33e0d9b0ea587f0d8f73
GHAZAL <you@example.com>
2023-01-29 22:58:36 +0300

    coffee break

---
+++ for_loop_2.py
@@ -0,0 +1,7 @@
+todo = ["exercise1", "exercise2", "exercise3","coffee break" ,"exercise4","exercise5","exercise6"]
+for x in todo:
+    if x == "COFFEE BREAK".lower():
+        print('it is', x ,'time')
+        break
+       
+  
